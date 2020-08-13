267fab3416e3ba061838c06a7814a3a037a67c6f
Valeriia <lera-bezborodova@mail.ru>
2020-08-13 23:03:07 +0300

    changed Test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -12,27 +12,27 @@ public class CashbackHackServiceTest {
     public void shouldBuyIfLess1000(){
         int actual = cashback.remain(800);
         int expected = 200;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void shouldBuyIfMore1000(){
         int actual = cashback.remain(1200);
         int expected = 800;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void shouldBuyIf0(){
         int actual = cashback.remain(0);
         int expected = 1000;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void shouldBuyIf1000(){
         int actual = cashback.remain(1000);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
